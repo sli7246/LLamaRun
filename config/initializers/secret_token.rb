d3512fbccff4c21b403514c72bafc3209e0180eb
John Li <johnsli@msn.com>
2013-09-06 22:01:35 -0400

    Initializing repository for LLamaRun

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LLamaRun::Application.config.secret_token = 'e7700506abbd0ec27c16a074d82f9544522295cd18bfbc8eef5f5e86c1c9fc0ee0264ff2ad2c7105bd625cfba4c48cc6070194de0ec4635432c4927373c76de0'
